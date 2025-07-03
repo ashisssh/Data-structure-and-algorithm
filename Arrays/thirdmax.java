99cf95be7cd228f37fb52fb268e18ffbc4d2df90
ashisssh <ashish91090009@gmail.com>
2025-07-03 02:59:44 -0700

    third max of an array

---
+++ Arrays/thirdmax.java
@@ -0,0 +1,26 @@
+public class thirdmax{
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,5};
+        thirdmaximum(arr);
+    }
+
+    public static void thirdmaximum(int arr[]){
+        int max = Integer.MIN_VALUE;
+        int secondmax = Integer.MIN_VALUE;
+        int thirdmax1 = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+              thirdmax1 = secondmax;
+                secondmax = max;
+                max = arr[i];
+            }
+            else if(arr[i]<max && arr[i] > secondmax){
+                secondmax =  arr[i];
+            }
+            else if(arr[i]>thirdmax1 && arr[i] < secondmax){
+              thirdmax1 =  arr[i];
+            }
+        }
+        System.out.println(thirdmax1);
+    }
+}
